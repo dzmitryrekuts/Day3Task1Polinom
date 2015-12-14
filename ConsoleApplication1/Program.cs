5c0b1834c20e9742884bdbe2ee304d5459491dcb
Dzmitry Rekuts <dzmitry_rekuts@epam.com>
2015-12-14 18:31:56 +0300

    console tests

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             var polinom = new Polinom(1, 1, -5, 20, 6.6, -76, 7, 738);
-            var polinom2 = new Polinom(1, 3, -5, 19, 6.5, -76, 57, 78);
+            var polinom2 = new Polinom(0, 50, 4, 3, -5, 19, 6.5, -76, 57, 78);
 
 
             Console.WriteLine(polinom.ToString());
